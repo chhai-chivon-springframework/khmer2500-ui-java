d012909acc720c2d9611254d0f50a448eaf285d5
chhaichivon <chivonchhai@hotmail.com>
2017-06-10 07:54:16 +0700

    khmer2500 ui

---
+++ src/main/java/com/khmer2500/ui/Khmer2500UiJavaApplication.java
@@ -0,0 +1,12 @@
+package com.khmer2500.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Khmer2500UiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Khmer2500UiJavaApplication.class, args);
+	}
+}
